ae86090640141123d98b711298023e0c6036422a
discip <53649486+discip@users.noreply.github.com>
2023-03-13 22:16:08 +0100

    Update configuration.h

--- source/Core/BSP/Pinecilv2/configuration.h
+++ source/Core/BSP/Pinecilv2/configuration.h
@@ -65,7 +65,7 @@
  */
 #define MIN_BRIGHTNESS             1   // Min OLED brightness selectable
 #define MAX_BRIGHTNESS             101 // Max OLED brightness selectable
-#define DEFAULT_BRIGHTNESS         25  // default OLED brightness
+#define DEFAULT_BRIGHTNESS         26  // default OLED brightness
 
 /**
  * Temp change settings
